4fdb878d54011d3c42ac6c4fc10cc98916233abb
Edward Pasenidis <me@edpasenidis.tech>
2020-07-06 17:57:09 +0300

    feat: registration plugin boilerplate

---
+++ regplugin/src/main/java/tapaidia/App.java
@@ -0,0 +1,14 @@
+package tapaidia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
